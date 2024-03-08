de3881ebac0c03cd3b43dc2890998def5945e6f6
githubercrazy <parunashvilidachi1@gmail.com>
2024-03-08 18:04:21 +0400

    some more changes with root

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
   const liElements = document.querySelectorAll("ul li");
 
-  // Example with specific href values
-   const hrefValues = ["#home", "#about", "#services", "#Professions", "#craftsmen"];
+  const hrefValues = [
+    "#home",
+    "#about",
+    "#services",
+    "#Professions",
+    "#craftsmen",
+  ];
 
   liElements.forEach(function (liElement, index) {
     const aElement = document.createElement("a");
     aElement.setAttribute("href", hrefValues[index]);
     aElement.className = "nav-item";
 
-    // Copy over the content from <li> to <a>
     while (liElement.firstChild) {
       aElement.appendChild(liElement.firstChild);
     }
 
-    // Replace <li> with <a>
     liElement.parentNode.replaceChild(aElement, liElement);
   });
 });
